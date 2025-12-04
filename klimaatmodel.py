7864ef73f622f1f5081f9c5c7988fbe83b67ee9d
DaanJordan <daanvanhonschoten@gmail.com>
2025-12-04 22:02:46 +0100

    Reformat code for klimaatmodel.py

--- klimaatmodel.py
+++ klimaatmodel.py
@@ -26,8 +26,8 @@ st.divider()
 st.subheader("Berekening van $T^*(t)$")
 
 
-        GHG = beta0 * Y0**beta2
-        st.markdown(f"## **{GHG:.3f}")
+GHG = beta0 * Y0**beta2
+st.markdown(f"## **{GHG:.3f}")
 
 # BEREKENING VAN T*(t)
 try:
@@ -71,6 +71,7 @@ except ZeroDivisionError:
 except Exception as e:
     st.error(f"Er is een onverwachte fout opgetreden: {e}")
     st.error("Controleer of de berekeningen positieve waarden geven voor de logaritme.")
+
 
 
 
