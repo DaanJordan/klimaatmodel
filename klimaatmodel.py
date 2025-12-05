cc34f452bb9633890924d6182180ba046065a586
DaanJordan <daanvanhonschoten@gmail.com>
2025-12-05 14:00:17 +0100

    Update klimaatmodel.py

--- klimaatmodel.py
+++ klimaatmodel.py
@@ -9,12 +9,12 @@ st.header("Input Parameters")
 # INPUTS
 Y0 = st.number_input("Y(0): NEGEER DIT! Inkomen in basisjaar", value=100.0, step=1.0, key="Y0_input")
 beta0 = st.number_input("NEGEER DIT! β0", value=1.0, step=0.1, format="%.1f", key="beta0_input")
-T0 = st.number_input("T(0): Temperatuur in basisjaar", value=0.56, step=0.01, format="%.2f", key="T0")
+T0 = st.number_input("T(0): Temperatuur in basisjaar", value=0.56, step=0.01, format="%.3f", key="T0")
 NGHG0 = st.number_input("NGHG(0): Emissies in basisjaar", value=0.09705, step=0.001, format="%.5f", key="NGHG0")
 delta = st.number_input("δ: schadeparameter", value=0.040, step=0.01, format="%.3f", key="delta")
-beta2 = st.number_input("1-β2: Innovatie en populatie parameter", value=0.52, step=0.01, format="%.2f", key="beta2")
+beta2 = st.number_input("1-β2: Innovatie en populatie parameter", value=0.52, step=0.01, format="%.3f", key="beta2")
 g = st.number_input("g: groeifactor", value=0.015, step=0.0288, format="%.3f", key="g")
-k = st.number_input("K: klimaatrespons op emissies", value=0.346, step=0.01, format="%.2f", key="k")
+k = st.number_input("K: klimaatrespons op emissies", value=0.346, step=0.01, format="%.3f", key="k")
 beta1 = st.number_input("β1: Innovatie en populatie parameter", value=0.030, step=0.001, format="%.3f", key="beta1")
 
 # Tijd variabele
@@ -96,6 +96,7 @@ else:
     
     # Plot de grafiek
     st.line_chart(df, x='Jaar', y='Temperatuur (°C)')
+
 
 
 
